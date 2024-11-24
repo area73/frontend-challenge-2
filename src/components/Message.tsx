9eb7c71ea42e639f57711101e635c5e21bc4e8c4
rerades <rerades@siete3.com>
2024-11-24 13:57:45 +0100

    adding animation

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,15 +1,19 @@
 import type { MessageProps } from '../types/components';
+import { motion } from 'framer-motion';
 
 export const Message = ({ text, author }: MessageProps) => {
   const isBot = author === 'bot';
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, x: isBot ? -40 : 40 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: isBot ? 2 : 0 }} // Delay de 2 segundos
       role="listitem"
       className={`${
         isBot ? 'bg-blue-500 text-white' : 'bg-green-500 text-white'
       } px-4 py-2 rounded-lg max-w-sm ${isBot ? 'self-start' : 'self-end ml-auto'}`}
     >
       {text}
-    </div>
+    </motion.div>
   );
 };
